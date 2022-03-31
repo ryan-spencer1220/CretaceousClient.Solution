d46cae159e1e155ae1a23915f904ff6114c2681e
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-31 10:07:39 -0700

    Edit HomeController and LaunchSettings
    Co-authored-by: Eric Crudup <Cruduper@users.noreply.github.com>
    Co-authored-by: Riley <RileyShimp@users.noreply.github.com>

--- CretaceousClient/Controllers/HomeController.cs
+++ CretaceousClient/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace CretaceousClient.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Animals");
         }
 
         public IActionResult Privacy()
